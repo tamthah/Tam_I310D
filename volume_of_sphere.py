a64662d3f6d5f636e858b5250eb2fe3d68d16d54
Tam Nguyen <168005804+tamthah@users.noreply.github.com>
2026-04-08 22:10:43 -0500

    Add function to calculate volume of a sphere

---
+++ volume_of_sphere.py
@@ -0,0 +1,7 @@
+def calculate_volume_of_sphere(radius):
+    pi = 3.14
+    volume = (4/3) * pi * radius * radius * radius
+    return volume
+
+print(calculate_volume_of_sphere(30))
+print(calculate_volume_of_sphere(40))
